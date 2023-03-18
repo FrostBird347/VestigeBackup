38f965fe7b487008d2b273fdbe6540fae24ad50e
FrostBird347 <39435218+FrostBird347@users.noreply.github.com>
2023-03-18 20:34:59 +0800

    Renamed file

--- Update.sh
+++ Update.sh
@@ -5,12 +5,12 @@ rm ./LatestDL.csv
 DownloadID="$(cat DownloadID)"
 wget "https://docs.google.com/spreadsheet/ccc?key=$DownloadID&output=csv" -O "./LatestDL.csv" --no-verbose
 
-OldLineCount="$(wc -l ./VestigeDownload.csv  | awk '{ print $1 }')"
-printf "$(cat VestigeDownload.csv)\n-----NEXTFILE-----\n$(cat LatestDL.csv)" | jsc ./merge.js > Merge.csv
+OldLineCount="$(wc -l ./VestigeBackup.csv  | awk '{ print $1 }')"
+printf "$(cat VestigeBackup.csv)\n-----NEXTFILE-----\n$(cat LatestDL.csv)" | jsc ./merge.js > Merge.csv
 
 rm ./LatestDL.csv
-cat ./Merge.csv > VestigeDownload.csv
+cat ./Merge.csv > VestigeBackup.csv
 rm ./Merge.csv
 
-git add ./VestigeDownload.csv
-git commit -m "Updated backup: $OldLineCount -> $(wc -l ./VestigeDownload.csv  | awk '{ print $1 }')"
+git add ./VestigeBackup.csv
+git commit -m "Updated backup: $OldLineCount -> $(wc -l ./VestigeBackup.csv  | awk '{ print $1 }')"
