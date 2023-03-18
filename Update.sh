b928abff1abb81c27184c21981643742234d9771
FrostBird347 <39435218+FrostBird347@users.noreply.github.com>
2023-03-18 20:19:48 +0800

    Setup

--- Update.sh
+++ Update.sh
@@ -5,11 +5,12 @@ rm ./LatestDL.csv
 DownloadID="$(cat DownloadID)"
 wget "https://docs.google.com/spreadsheet/ccc?key=$DownloadID&output=csv" -O "./LatestDL.csv" --no-verbose
 
-OldLineCount="$(wc -l ./VestigeDownload.csv)"
-
+OldLineCount="$(wc -l ./VestigeDownload.csv  | awk '{ print $1 }')"
 printf "$(cat VestigeDownload.csv)\n-----NEXTFILE-----\n$(cat LatestDL.csv)" | jsc ./merge.js > Merge.csv
+
+rm ./LatestDL.csv
 cat ./Merge.csv > VestigeDownload.csv
 rm ./Merge.csv
 
 git add ./VestigeDownload.csv
-git commit -m "Updated backup: $OldLineCount -> $(wc -l ./VestigeDownload.csv)"
+git commit -m "Updated backup: $OldLineCount -> $(wc -l ./VestigeDownload.csv  | awk '{ print $1 }')"
